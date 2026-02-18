de48d6fe1355396bd6d9c37ade1f31e7af8a8df4
See2et <git@see2et.dev>
2026-02-19 05:42:41 +0900

    fix(home/desktop): prefer ALVR hardware encoding defaults

--- home/desktop/alvr.nix
+++ home/desktop/alvr.nix
@@ -93,15 +93,15 @@
     startupNotify = true;
   };
 
-  home.activation.alvrForceSoftwareEncoding = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation.alvrPreferHardwareEncoding = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     session_file="${config.home.homeDirectory}/.config/alvr/session.json"
 
     if [ -f "$session_file" ]; then
       tmp_file="$(${pkgs.coreutils}/bin/mktemp)"
 
       if ${pkgs.jq}/bin/jq '
-        .openvr_config.force_sw_encoding = true
-        | .session_settings.video.encoder_config.software.force_software_encoding = true
+        .openvr_config.force_sw_encoding = false
+        | .session_settings.video.encoder_config.software.force_software_encoding = false
       ' "$session_file" > "$tmp_file"; then
         ${pkgs.coreutils}/bin/mv "$tmp_file" "$session_file"
       else
