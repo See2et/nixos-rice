787341cac817a38eade052010c8788a3a4b188df
See2et <git@see2et.dev>
2026-02-18 18:26:54 +0900

    fix(niri): force gtk xdg portal backend for file chooser

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -4,6 +4,15 @@
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri;
 
-  xdg.portal.enable = true;
-  xdg.portal.xdgOpenUsePortal = true;
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+    config.common = {
+      default = [ "gtk" ];
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+    };
+  };
 }
