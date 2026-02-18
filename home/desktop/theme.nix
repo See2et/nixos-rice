69e890fd4f85532708c6ad7a88e5544137bc7396
See2et <git@see2et.dev>
2026-02-18 22:04:50 +0900

    feat(home/desktop): unify gtk/icon/cursor/qt theming
    Consolidate cross-toolkit theme settings in the desktop theme module and record T2-1 completion for the new unified baseline.

--- home/desktop/theme.nix
+++ home/desktop/theme.nix
@@ -1,4 +1,11 @@
-{ ... }:
+{ lib, pkgs, ... }:
+let
+  inherit (lib) mkForce;
+  gtkTheme = "catppuccin-frappe-blue-standard";
+  iconTheme = "Papirus-Dark";
+  cursorTheme = "Bibata-Modern-Ice";
+  cursorSize = 24;
+in
 {
   dconf.settings."org/gnome/desktop/interface" = {
     color-scheme = "prefer-dark";
@@ -6,13 +13,46 @@
 
   gtk = {
     enable = true;
+    theme = {
+      name = mkForce gtkTheme;
+      package = mkForce pkgs.catppuccin-gtk;
+    };
+    iconTheme = {
+      name = mkForce iconTheme;
+      package = mkForce pkgs.papirus-icon-theme;
+    };
+    cursorTheme = {
+      name = mkForce cursorTheme;
+      package = mkForce pkgs.bibata-cursors;
+    };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+  };
+
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = cursorTheme;
+    size = cursorSize;
+    gtk.enable = true;
+    x11.enable = true;
   };
 
   qt = {
     enable = true;
     platformTheme.name = "gtk3";
-    style.name = "kvantum";
+    style = {
+      name = "kvantum";
+      package = pkgs.libsForQt5.qtstyleplugin-kvantum;
+    };
   };
+
+  home.packages = [
+    pkgs.catppuccin-kvantum
+    pkgs.qt6Packages.qtstyleplugin-kvantum
+  ];
+
+  xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
+    [General]
+    theme=catppuccin-frappe-blue
+  '';
 }
