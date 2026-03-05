ae545787d8a8e4da029cfa4f0728455715eb2f84
See2et <git@see2et.dev>
2026-03-06 01:03:40 +0900

    refactor(home/linux): remove unused module arguments

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -2,7 +2,7 @@
 # Settings common to all Linux targets (desktop + WSL) but not Darwin
 # Packages that are Linux-only go here
 
-{ config, pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     wl-clipboard
