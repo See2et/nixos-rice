ce0f1aaf063095c41643e59811c0bff2fdcb5a3a
See2et <git@see2et.dev>
2026-03-06 01:03:28 +0900

    feat(home/common): export PKG_CONFIG_PATH in session variables

--- home/common/session.nix
+++ home/common/session.nix
@@ -1,6 +1,17 @@
 # Common session variables (platform-agnostic)
 # WSL-specific PATH (/mnt/c/...) belongs in home/wsl
-{ config, ... }:
+{ config, isDarwin, lib, ... }:
+let
+  pkgConfigPathEntries =
+    [
+      "${config.home.profileDirectory}/lib/pkgconfig"
+      "${config.home.profileDirectory}/share/pkgconfig"
+    ]
+    ++ lib.optionals (!isDarwin) [
+      "/run/current-system/sw/lib/pkgconfig"
+      "/run/current-system/sw/share/pkgconfig"
+    ];
+in
 {
   home.sessionVariables = {
     EDITOR = "nvim";
@@ -8,6 +19,7 @@
     SUDO_EDITOR = "nvim";
     UV_TOOL_DIR = "${config.xdg.dataHome}/uv/tools";
     UV_TOOL_BIN_DIR = "${config.xdg.dataHome}/uv/tools/bin";
+    PKG_CONFIG_PATH = lib.concatStringsSep ":" pkgConfigPathEntries;
   };
 
   home.sessionPath = [
