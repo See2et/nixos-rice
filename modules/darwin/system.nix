b62be99e1921d29449da9360d1d5d2c29454c18d
See2et <git@see2et.dev>
2026-05-08 12:42:30 +0900

    feat(darwin): add declarative system defaults

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -18,5 +18,67 @@
     pnpm
   ];
 
+  system.defaults = {
+    CustomSystemPreferences = {
+      NSGlobalDomain = {
+        AppleMiniaturizeOnDoubleClick = false;
+      };
+
+      "com.apple.finder" = {
+        ShowSidebar = true;
+      };
+    };
+
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      NSAutomaticCapitalizationEnabled = true;
+      NSAutomaticPeriodSubstitutionEnabled = true;
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.springing.delay" = 0.5;
+      "com.apple.springing.enabled" = true;
+      "com.apple.trackpad.forceClick" = true;
+      "com.apple.trackpad.scaling" = 3.0;
+    };
+
+    dock = {
+      autohide = true;
+      tilesize = 62;
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;
+    };
+
+    finder = {
+      FXPreferredViewStyle = "clmv";
+      FXRemoveOldTrashItems = true;
+      ShowExternalHardDrivesOnDesktop = true;
+      ShowHardDrivesOnDesktop = false;
+      ShowRemovableMediaOnDesktop = true;
+    };
+
+    menuExtraClock = {
+      ShowAMPM = true;
+      ShowDate = 0;
+      ShowDayOfWeek = true;
+    };
+
+    trackpad = {
+      Clicking = true;
+      TrackpadRightClick = true;
+      TrackpadThreeFingerDrag = true;
+      TrackpadFourFingerHorizSwipeGesture = 2;
+      TrackpadFourFingerVertSwipeGesture = 2;
+      TrackpadMomentumScroll = true;
+      TrackpadPinch = true;
+      TrackpadRotate = true;
+      TrackpadThreeFingerHorizSwipeGesture = 2;
+      TrackpadThreeFingerVertSwipeGesture = 2;
+      TrackpadTwoFingerFromRightEdgeSwipeGesture = 3;
+    };
+  };
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
   programs.zsh.enable = true;
 }
