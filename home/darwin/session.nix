68496a342735429669d8361504d7dbb9f536e27a
See2et <git@see2et.dev>
2026-05-14 17:59:08 +0900

    refactor(darwin): reuse shared typst font path helper
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- home/darwin/session.nix
+++ home/darwin/session.nix
@@ -2,11 +2,13 @@
 { config, lib, ... }:
 {
   home.sessionVariables = {
-    TYPST_FONT_PATHS = lib.concatStringsSep ":" [
-      "${config.xdg.dataHome}/fonts"
-      "/System/Library/Fonts"
-      "/Library/Fonts"
-      "${config.home.homeDirectory}/Library/Fonts"
-    ];
+    TYPST_FONT_PATHS = import ../../shared/typst-font-paths.nix {
+      inherit lib config;
+      extraPaths = [
+        "/System/Library/Fonts"
+        "/Library/Fonts"
+        "${config.home.homeDirectory}/Library/Fonts"
+      ];
+    };
   };
 }
