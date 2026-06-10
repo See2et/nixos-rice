8b790c715fa613c81c83baf978e94109dd3c3cdb
See2et <git@see2et.dev>
2026-06-11 03:37:35 +0900

    build: update flake inputs and pin docker to version 29

--- modules/nixos/desktop/docker.nix
+++ modules/nixos/desktop/docker.nix
@@ -1,10 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  dockerPackage = pkgs.docker_29;
+in
 {
   virtualisation.docker = {
     enable = true;
+    package = dockerPackage;
     rootless = {
       enable = true;
       setSocketVariable = true;
+      package = dockerPackage;
     };
   };
 }
