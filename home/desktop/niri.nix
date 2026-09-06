43ddc1514bcf2fdb94cb9908db50006d92a59aab
See2et <git@see2et.dev>
2026-09-06 17:12:57 +0900

    feat(niri): add numeric workspace focus keybindings

--- home/desktop/niri.nix
+++ home/desktop/niri.nix
@@ -23,6 +23,15 @@ in
         "Mod+L".action.focus-column-right = [ ];
         "Mod+U".action.focus-workspace-down = [ ];
         "Mod+I".action.focus-workspace-up = [ ];
+        "Mod+1".action.focus-workspace = 1;
+        "Mod+2".action.focus-workspace = 2;
+        "Mod+3".action.focus-workspace = 3;
+        "Mod+4".action.focus-workspace = 4;
+        "Mod+5".action.focus-workspace = 5;
+        "Mod+6".action.focus-workspace = 6;
+        "Mod+7".action.focus-workspace = 7;
+        "Mod+8".action.focus-workspace = 8;
+        "Mod+9".action.focus-workspace = 9;
 
         "Mod+Comma".action.consume-window-into-column = [ ];
         "Mod+Period".action.expel-window-from-column = [ ];
